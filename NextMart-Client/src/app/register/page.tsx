6a4719e8538ee3800d7de5f244f4d81b187ef711
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2025-03-01 02:18:19 +0600

    User Registration with Server Actions

---
+++ NextMart-Client/src/app/register/page.tsx
@@ -0,0 +1,11 @@
+import RegisterFrom from "@/components/modules/auth/register/RegisterFrom";
+
+const RegisterPage = () => {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center">
+      <RegisterFrom />
+    </div>
+  );
+};
+
+export default RegisterPage;
