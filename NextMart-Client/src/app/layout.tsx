dee690c66c23e2bb9f069441b40493c4485f79ef
Kamanashis-Biswas <kamanashis.cse@gmail.com>
2025-03-01 01:17:18 +0600

    Setting Up User and Admin Layouts with Route Groups

--- NextMart-Client/src/app/layout.tsx
+++ NextMart-Client/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,13 +23,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-      <html lang="en">
-        <body
-          className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-        >
-          <Toaster richColors position="top-center" />
-          {children}
-        </body>
-      </html>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
   );
 }
